318edd40445920cabbbdb57079312d765bc5c8c1
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-06-03 10:16:54 +0200

    crud Photo

---
+++ src/Form/PhotoType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Photo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PhotoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Photo::class,
+        ]);
+    }
+}
