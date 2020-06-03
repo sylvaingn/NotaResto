24a3f10db23d4be91e272d0da9341df24bf3227d
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-06-03 11:48:34 +0200

    maj Faker restaurant

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,28 @@
 <?php
-
+// src/DataFixtures/FakerFixtures.php
 namespace App\DataFixtures;
 
+use App\Entity\Restaurant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        // On configure dans quelles langues nous voulons nos données
+        $faker = Faker\Factory::create('fr_FR');
+
+        // on créé 10 restaurants
+        for ($i = 0; $i < 10; $i++) {
+            $restaurant = new Restaurant();
+            $restaurant->setNom($faker->company);
+            $restaurant->setDescription($faker->text);
+            $restaurant->setCreatedAt($faker->dateTime);
+            $manager->persist($restaurant);
+        }
 
         $manager->flush();
     }
